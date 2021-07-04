ffc68a640649bc8a471433a7499a5cbeb562b48b
elgrunt0 <grunt@G10.localdomain>
2021-07-04 23:29:20 +0100

    fixing end of script

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
@@ -31,5 +31,6 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-cd ..
-source ~/.bashrc
+cd ~/
+source .bashrc
+
